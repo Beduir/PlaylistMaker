55a33644e244637d1e657d97b35b144689ef83e5
Beduir <beduir@gmail.com>
2023-01-27 15:05:04 +0400

    .idea removed

--- app/src/main/java/com/beduir/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/beduir/playlistmaker/SettingsActivity.kt
@@ -1,12 +1,15 @@
 package com.beduir.playlistmaker
 
+import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +20,9 @@ class SettingsActivity : AppCompatActivity() {
         val shareButton = findViewById<TextView>(R.id.share)
         val supportButton = findViewById<TextView>(R.id.support)
         val userAgreementButton = findViewById<TextView>(R.id.user_agreement)
+        var darkThemeSwitch = findViewById<Switch>(R.id.dark_theme)
+
+        darkThemeSwitch.isChecked = getDarkThemeState()
 
         backButton.setOnClickListener {
             finish()
@@ -43,5 +49,18 @@ class SettingsActivity : AppCompatActivity() {
             shareIntent.data = Uri.parse(this.getString(R.string.user_agreement_url))
             startActivity(shareIntent)
         }
+
+        darkThemeSwitch.setOnCheckedChangeListener { button, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+            }
+        }
+    }
+
+    private fun getDarkThemeState(): Boolean {
+        return resources.configuration.uiMode and
+                Configuration.UI_MODE_NIGHT_YES == Configuration.UI_MODE_NIGHT_YES
     }
 }
