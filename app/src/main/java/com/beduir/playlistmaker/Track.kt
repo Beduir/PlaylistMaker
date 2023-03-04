c09aea2649123afc8a929ce8b7ebf6a108575d25
Beduir <beduir@gmail.com>
2023-03-04 17:30:43 +0400

    Sprint 13

--- app/src/main/java/com/beduir/playlistmaker/Track.kt
+++ app/src/main/java/com/beduir/playlistmaker/Track.kt
@@ -1,12 +1,18 @@
 package com.beduir.playlistmaker
 
+import java.io.Serializable
+
 data class Track(
     val trackId: Int,
     val trackName: String,
     val artistName: String,
     val trackTimeMillis: Int,
     val artworkUrl100: String,
-)
+    val collectionName: String,
+    val releaseDate: String,
+    val primaryGenreName: String,
+    val country: String
+): Serializable
 
 class TracksResponse(
     val results: List<Track>
